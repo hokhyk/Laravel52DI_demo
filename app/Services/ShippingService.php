be1a98426347564503a41e6c114d9c317f4491ac
oomusou <oomusou@gmail.com>
2016-04-17 16:56:20 +0800

    將ShippingService重構成method injection

--- app/Services/ShippingService.php
+++ app/Services/ShippingService.php
@@ -4,24 +4,13 @@ namespace App\Services;
 
 class ShippingService
 {
-    /** @var LogisticsInterface */
-    private $logistics;
-
     /**
-     * ShippingService constructor.
      * @param LogisticsInterface $logistics
-     */
-    public function __construct(LogisticsInterface $logistics)
-    {
-        $this->logistics = $logistics;
-    }
-
-    /**
      * @param int $weight
      * @return int
      */
-    public function calculateFee($weight)
+    public function calculateFee(LogisticsInterface $logistics, $weight)
     {
-        return $this->logistics->calculateFee($weight);
+        return $logistics->calculateFee($weight);
     }
 }
