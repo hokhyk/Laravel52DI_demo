21260f19a09380370f13c26934107ce954d5114c
oomusou <oomusou@gmail.com>
2016-04-17 15:49:13 +0800

    新增ShippingService的單元測試

---
+++ tests/Services/ShippingServiceTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Services\BlackCat;
+use App\Services\LogisticsInterface;
+use App\Services\ShippingService;
+
+class ShippingServiceTest extends TestCase
+{
+    /** @test */
+    public function 黑貓單元測試()
+    {
+        /** arrange */
+        $expected = 100;
+        $weight = 1;
+
+        $mock = Mockery::mock(LogisticsInterface::class);
+        $mock->shouldReceive('calculateFee')
+            ->once()
+            ->withAnyArgs()
+            ->andReturn($expected);
+
+        App::instance(LogisticsInterface::class, $mock);
+
+        $target = App::make(ShippingService::class);
+
+        /** act */
+        $actual = $target->calculateFee($weight);
+
+        /** assert */
+        $this->assertEquals($expected, $actual);
+    }
+}
